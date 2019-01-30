2bede74511b7db44ebaacdfadacb8df585a59fd5
Sina Kashi Pazha <esterlinkof@gmail.com>
2019-01-30 19:57:14 +0330

    Refactore comband.

--- comband.py
+++ comband.py
@@ -1,21 +1,21 @@
 import random
 import itertools
+import math
 import numpy as np
+from scipy.special import binom
+
 
 class ComBand():
 	def __init__(self,gamma=0.3,K=[1,2,3],k=1):
 		self.weights = []
 		self.probs = []
-		self.actions = []
 		self.gamma = gamma
 		self.k = k
 		self.K = K
-		self.CKk = choose(len(K),k)
+		self.CKk = int(binom(len(K),k))
 		self.C = self.gamma / self.CKk
-		self.prob_updated = False
 		self.actions = list(itertools.combinations(K , k))
 		self.oneUdot1UT = []
-
 
 		for i in range(0, self.CKk):
 			self.weights.append(1)
@@ -40,14 +40,7 @@ class ComBand():
 			print ("weight %d = %f and prob = %f for action -> %s" % (i,self.weights[i], self.probs[i], self.actions[i]) )
 
 	def next_action(self):
-		if (not self.prob_updated ):
-			update_probabilities()
-
-		action_index = random.choices(range(0,self.CKk),weights=self.probs)
-
-		self.prob_updated = False
-
-		return self.actions[action_index[0]]
+		return self.actions[random.choices(range(0,self.CKk),weights=self.probs)[0]]
 
 	def update_weights(self,rewards,action):
 		if ( len(rewards) != self.k ):
@@ -68,21 +61,3 @@ class ComBand():
 		for i in range(0,len(self.weights)) :
 			pr = (1 - self.gamma)*self.weights[i]/sum(self.weights) + self.C
 			self.probs[i] = pr
-
-		self.prob_updated = True
-
-
-def choose(n, k):
-    """
-    A fast way to calculate binomial coefficients by Andrew Dalke (contrib).
-    """
-    if 0 <= k <= n:
-        ntok = 1
-        ktok = 1
-        for t in range(1, min(k, n - k) + 1):
-            ntok *= n
-            ktok *= t
-            n -= 1
-        return ntok // ktok
-    else:
-        return 0
